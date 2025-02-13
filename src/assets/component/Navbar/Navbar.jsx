9ba45eabf6ad29d839cdef981ab96fcaf23dbb4e
tarek-mahall <tarekmahall32@gmail.com>
2025-02-13 12:10:50 +0200

    Handel NavBar

--- src/assets/component/Navbar/Navbar.jsx
+++ src/assets/component/Navbar/Navbar.jsx
@@ -1,45 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../freshcart-logo.svg';
-import { Link, NavLink } from 'react-router-dom';
-import { FaFacebook, FaTiktok, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa';
-import { CiLinkedin } from 'react-icons/ci';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTiktok, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 export default function Navbar() {
-  const socialMediaLinks = [
-    { icon: <FaFacebook />, url: 'https://facebook.com', target: '_blank' },
-    { icon: <FaTiktok />, url: 'https://tiktok.com', target: '_blank' },
-    { icon: <FaTwitter />, url: 'https://twitter.com', target: '_blank' },
-    { icon: <FaInstagram />, url: 'https://instagram.com', target: '_blank' },
-    { icon: <CiLinkedin />, url: 'https://linkedin.com', target: '_blank' },
-    { icon: <FaYoutube />, url: 'https://youtube.com', target: '_blank' },
-  ];
-  const navLinks = [
-    { to: '/', text: 'Home', hiddenOnMobile: true },
-    { to: '/cart', text: 'Cart' },
-    { to: '/products', text: 'Products' },
-    { to: '/categories', text: 'Categories' },
-    { to: '/brands', text: 'Brands' },
-  ];
-
-  const authLinks = [
-    { to: '/login', text: 'Login' },
-    { to: '/register', text: 'Register' },
-    { to: '/login', text: 'SignOut', hiddenOnMobile: true },
-  ];
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token'); 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+  const handleLogout = () => {
+    localStorage.removeItem('token'); 
+    navigate('/login'); 
+    window.location.reload();
+  };
   return (
-    <nav className="top-0 left-0 right-0 bg-white border-gray-200 dark:bg-gray-900 z-50">
-      <div className="max-w-screen-xl flex flex-wrap justify-between items-center mx-auto p-4">
-        <div className="flex items-center space-x-3 rtl:space-x-reverse">
-          <Link to="/" className="flex items-center">
-            <img src={logo} className="h-8" alt="FreshCart Logo" />
+    <nav className="bg-white">
+      <div className="w-5/6 flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center justify-between w-full xl:w-auto">
+          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logo} className="h-8" alt="Logo Site" />
           </Link>
+
           <button
-            data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
             aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen}
+            onClick={toggleMenu}
           >
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -47,42 +39,143 @@ export default function Navbar() {
             </svg>
           </button>
         </div>
-        <div className="hidden w-full md:flex md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            {navLinks.map((link, index) => (
-              <li key={index} className={link.hiddenOnMobile ? 'hidden md:block' : ''}>
-                <NavLink
-                  to={link.to}
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  {link.text}
-                </NavLink>
-              </li>
-            ))}
+
+        <div
+          className={`${
+            isMenuOpen ? 'max-h-screen' : 'max-h-0'
+          } w-full xl:max-h-screen xl:w-auto transition-all duration-300 ease-in-out overflow-hidden`}
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col p-4 xl:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 xl:flex-row xl:space-x-8 rtl:space-x-reverse xl:mt-0 xl:border-0 xl:bg-transparent">
+            {token && (
+              <>
+                <li>
+                  <NavLink
+                    to="/"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    aria-current="page"
+                    onClick={closeMenu}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/cart"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    onClick={closeMenu}
+                  >
+                    Cart
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/products"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    onClick={closeMenu}
+                  >
+                    Products
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/categories"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    onClick={closeMenu}
+                  >
+                    Categories
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/brands"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    onClick={closeMenu}
+                  >
+                    Brands
+                  </NavLink>
+                </li>
+              </>
+            )}
+
+            {!token && (
+              <>
+                <li className="xl:hidden">
+                  <NavLink
+                    to="/login"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    onClick={closeMenu}
+                  >
+                    Login
+                  </NavLink>
+                </li>
+                <li className="xl:hidden">
+                  <NavLink
+                    to="/register"
+                    className="block py-2 px-3 text-black hover:bg-blue-800 xl:hover:bg-transparent xl:hover:text-blue-800 xl:p-0"
+                    onClick={closeMenu}
+                  >
+                    Register
+                  </NavLink>
+                </li>
+              </>
+            )}
           </ul>
         </div>
-        <div className="hidden w-full md:flex md:w-auto" id="navbar-social">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            {socialMediaLinks.map((link, index) => (
-              <li key={index}>
-                <a href={link.url} target={link.target} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                  {link.icon}
-                </a>
-              </li>
-            ))}
-            {authLinks.map((link, index) => (
-              <li key={index} className={link.hiddenOnMobile ? 'hidden md:block' : ''}>
-                <NavLink
-                  to={link.to}
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+
+        <div className="hidden xl:w-auto xl:flex items-center space-x-4">
+          <div className="flex space-x-4">
+            <a href="https://web.facebook.com/" target="_blank" className="hover:text-blue-800 text-xl">
+              <FaFacebook />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" className="hover:text-blue-800 text-xl">
+              <FaInstagram />
+            </a>
+            <a href="https://www.tiktok.com/" target="_blank" className="hover:text-blue-800 text-xl">
+              <FaTiktok />
+            </a>
+            <a href="https://x.com/" target="_blank" className="hover:text-blue-800 text-xl">
+              <FaTwitter />
+            </a>
+            <a href="https://www.linkedin.com/" target="_blank" className="hover:text-blue-800 text-xl">
+              <FaLinkedin />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank" className="hover:text-blue-800 text-xl">
+              <FaYoutube />
+            </a>
+          </div>
+
+          <div className="flex space-x-4 text-black">
+            {token && (
+              <button
+                onClick={handleLogout}
+                className="text-black hover:text-blue-800"
+              >
+                Sign Out
+              </button>
+            )}
+
+            {!token && (
+              <>
+                <Link
+                  to="/login"
+                  className="text-black hover:text-blue-800"
                 >
-                  {link.text}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="text-black hover:text-blue-800"
+                >
+                  Register
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </nav>
   );
 }
+
+
